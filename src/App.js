f2933dbd2b0545b7d8695fa141d331694b97d74a
Suman_pokhrel <spokhrel196@gmail.com>
2023-03-30 09:44:17 +0545

    Header component completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h2>Suman Pokhrel building Gmail</h2>
+      <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import "./Header.css";
+
+
+import SearchIcon from '@mui/icons-material/Search';
+import Avatar from '@mui/material/Avatar';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import TuneIcon from '@mui/icons-material/Tune';
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton, Typography } from '@mui/material';
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+
+        <img
+          src="https://th.bing.com/th/id/R.f82954e97398fa6a025570c97249846f?rik=N1EQMq%2fK%2fE1ddA&pid=ImgRaw&r=0"
+          alt="" />
+
+        <Typography
+          variant='h5'
+          textColor='secondary'
+          ml={"5px"}
+        >Gmail</Typography>
+
+
+      </div>
+
+      <div className="header__middle">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input type="text" placeholder='Search mail' />
+        <IconButton>
+          <TuneIcon />
+        </IconButton>
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <HelpOutlineOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+
+        <IconButton>
+          <AppsOutlinedIcon />
+        </IconButton>
+
+        <IconButton>
+          <Avatar alt="Suman Pokhrel" src="" />
+
+        </IconButton>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Header
